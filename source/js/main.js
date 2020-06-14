34ef15684da7d7ee0487d41bc48a371bd3c95800
isyumich <59385945+Isyumich@users.noreply.github.com>
2020-06-14 15:07:14 +0300

    Для Трэвиса

--- source/js/main.js
+++ source/js/main.js
@@ -166,18 +166,17 @@ var capacity = document.querySelector('#capacity');
 var roomNumber = document.querySelector('#room_number');
 
 capacity.addEventListener('change', function () {
-    if (roomNumber.value === '100') {
+  if (roomNumber.value === '100') {
+    console.log('success');
+    if (capacity.value !== '0') {
       console.log('success');
-      if (capacity.value !== '0') {
-        console.log('success');
-        capacity.setCustomValidity('Сто комнат не для гостей');
-      } else {capacity.setCustomValidity('');
-      }
-    } else if (roomNumber.value < capacity.value) {
-      capacity.setCustomValidity('Количество гостей не может превышать количество комнат');
+      capacity.setCustomValidity('Сто комнат не для гостей');
     } else {
-      capacity.setCustomValidity('')
+      capacity.setCustomValidity('');
     }
-  });
-
-
+  } else if (roomNumber.value < capacity.value) {
+    capacity.setCustomValidity('Количество гостей не может превышать количество комнат');
+  } else {
+    capacity.setCustomValidity('');
+  }
+});
