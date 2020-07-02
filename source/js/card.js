6e7c411cffbb3e21820792c9bb9bbcd439ced709
Isyumich <59385945+Isyumich@users.noreply.github.com>
2020-07-02 19:34:00 +0300

    Исправления

--- source/js/card.js
+++ source/js/card.js
@@ -1,28 +1,11 @@
 'use strict';
 
 (function () {
-  var ELEMENT_WIDTH = 50;
-  var ELEMENT_HEIGHT = 70;
-
-// Функция создания метки
-  var pin = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-
-  var renderRentAnnouncement = function (rentalAnnouncement) {
-    var rentElement = pin.cloneNode(true);
-
-    rentElement.querySelector('img').src = rentalAnnouncement.author.avatar;
-    rentElement.querySelector('img').alt = rentalAnnouncement.offer.title;
-    rentElement.style.left = rentalAnnouncement.location.x - ELEMENT_WIDTH / 2 + 'px';
-    rentElement.style.top = rentalAnnouncement.location.y - ELEMENT_HEIGHT + 'px';
-
-    return rentElement;
-  };
-
+  // Функция создания карточки с описанием
   var card = document.querySelector('#card')
   .content
   .querySelector('.map__card');
+  var map = document.querySelector('.map');
 
   var renderRentDescription = function (rentalDescription) {
     var cardElement = card.cloneNode(true);
@@ -98,7 +81,6 @@
         closeCardButton.addEventListener('click', hiddenCardHandler(map));
       }
     };
-
   // Функция скрытия элемента
     var hiddenCardHandler = function (element) {
       return function (){
@@ -107,53 +89,7 @@
       }
     };
 
-// Функция рендеринга элементов
-  var mapPins = document.querySelector('.map__pins');
-  var map = document.querySelector('.map');
-
-  var renderAdElements = function () {
-    var successHandler = function (pins) {
-      var pinFragment = document.createDocumentFragment();
-      for (var j = 0; j < pins.length; j++) {
-        pinFragment.appendChild(renderRentAnnouncement(pins[j]));
+      window.card = {
+        adCardHandler: adCardHandler
       }
-
-      mapPins.appendChild(pinFragment);
-
-      var mapPinElements = document.querySelectorAll('.map__pin');
-
-      for (var l = 1; l < mapPinElements.length; l++) {
-        mapPinElements[l].addEventListener('click', adCardHandler(mapPinElements[l], pins[l-1]));
-
-        mapPinElements[l].addEventListener('keydown', function (evt) {
-          if (evt.key === 'Enter') {
-            adCardHandler(mapPinElements[l], pins[l-1]);
-          }
-        });
-  };
-  document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Escape') {
-      map.removeChild(document.querySelector('.map__card'));
-    }
-  });
-};
-
-
-    var errorHandler = function (errorMessage) {
-      var node = document.createElement('div');
-      node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-      node.style.position = 'absolute';
-      node.style.left = 0;
-      node.style.right = 0;
-      node.style.fontSize = '30px';
-
-      node.textContent = errorMessage;
-      document.body.insertAdjacentElement('afterbegin', node);
-    };
-
-    window.backend.load(successHandler, errorHandler);
-  };
-  window.pin = {
-    renderAdElements: renderAdElements
-  }
 })();
